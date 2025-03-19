86602ba9ad98fa1713aa99a2c3bb97c1c7697f9a
Kartik Gothwal <kartikgothwal28@gmail.com>
2025-03-19 14:32:33 +0530

    added the board and logic to put X or O

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [board, setBoard] = useState<Array<"X" | "O" | "">>(Array(9).fill(""));
+  const [isXNext, setIsXNext] = useState<boolean>(true);
+  const handleClick = (index: number) => {
+    if (board[index]) return;
+    const newBoard = [...board];
+    newBoard[index] = isXNext ? "X" : "O";
+    setBoard(newBoard);
+    setIsXNext(!isXNext);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="game">
+        <h1>Tic Tac Toe</h1>
+        <h1 className="my-2"> NextPlayer : {isXNext ? "X" : "O"}</h1>
+        <div className="board">
+          {board.map((item, index: number) => {
+            return (
+              <button
+                className="square"
+                key={index}
+                onClick={() => handleClick(index)}
+              >
+                {item}
+              </button>
+            );
+          })}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
